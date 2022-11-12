fae72a6d399642c165270d6b47a8fc6329970a2f
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-11-12 11:19:09 +0800

    Update lv_ctrl_fsm.sv

--- lv_ctrl_fsm.sv
+++ lv_ctrl_fsm.sv
@@ -1,7 +1,8 @@
 //============================================================
 //Module   : lv_ctrl_fsm
 //Function : low voltage die, ctrl fsm
-//File Tree: 
+//File Tree: lv_core
+//            |--lv_ctrl_fsm
 //-------------------------------------------------------------
 //Update History
 //-------------------------------------------------------------
@@ -12,7 +13,7 @@ module lv_ctrl_fsm #(
     `include "lv_param.vh"
     parameter END_OF_LIST          = 1
 )( 
-    input  logic           i_pwr_on        ,
+    input  logic           i_power_on      ,
     input  logic           i_test_mode     ,
     input  logic           i_efuse_vld     ,//load efuse's data to register, come frome register.
     input  logic           i_efuse_done    ,//come from register.	
@@ -31,50 +32,64 @@ module lv_ctrl_fsm #(
     input  logic           i_hv_oc_err     ,//hv over current err.
     input  logic           i_hv_desat_err  ,
     input  logic           i_hv_scp_err    ,//hv short circuit err.
-	
-	
-    input  logic           i_clk	      ,
+    input  logic           i_normal_en     ,
+    input  logic           i_cfg_en        ,
+    input  logic           i_bist_en       ,
+    input  logic           i_sft_rst       ,//soft reset
+    output logic           o_pwm_en        ,
+    output logic           o_crc_wdg_en    ,
+    output logic           o_ow_wdg_en     ,
+    output logic           o_spi_en        ,
+    output logic           o_fsc_en        ,//failsafe ctrl enb
+    output logic           o_int_n         ,//interupte
+		
+    input  logic           i_clk	   ,
     input  logic           i_rst_n
  );
 //==================================
 //local param delcaration
 //==================================
-
+localparam FSM_ST_NUM      = 11;
+localparam FSM_ST_W        = FSM_ST_NUM ? 1 : $clog2(FSM_ST_NUM);
+	
+localparam POWER_DOWN_ST   = FSM_ST_W'(0 );
+localparam WAIT_ST         = FSM_ST_W'(1 ); 
+localparam TEST_ST         = FSM_ST_W'(2 );
+localparam NORMAL_ST       = FSM_ST_W'(3 );
+localparam FAILSAFE_ST     = FSM_ST_W'(4 );
+localparam OW_COMM_ERR_ST  = FSM_ST_W'(5 );
+localparam OW_WDG_FAULT_ST = FSM_ST_W'(6 );
+localparam FAULT_ST        = FSM_ST_W'(7 );
+localparam CFG_ST          = FSM_ST_W'(8 );
+localparam RST_ST          = FSM_ST_W'(9 );
+localparam BIST_ST         = FSM_ST_W'(10);
 //==================================
 //var delcaration
 //==================================
-logic lvhv_err0;
+logic 			lvhv_err0;
+logic 			lvhv_err1;
+logic 			lvhv_err2;
+logic [FSM_ST_W-1: 0] 	cur_st	 ;
+logic [FSM_ST_W-1: 0] 	nxt_st	 ;
 //==================================
 //main code
 //==================================
-assign hit = (i_addr==REG_ADDR);
-assign wen = i_wen & hit & ((i_test_mode_status & SUPPORT_TEST_MODE_WR) | (i_cfg_mode_status & SUPPORT_CFG_MODE_WR));
-assign ren = i_ren & hit & ((i_test_mode_status & SUPPORT_TEST_MODE_RD) | (i_cfg_mode_status & SUPPORT_CFG_MODE_RD));
-  
 always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-         o_reg_data <= DEFAULT_VAL;
-	   crc_data <= {CRC_W{1'b0}};
-    end
-    else begin
-         o_reg_data <= wen ? i_wdata : o_reg_odata;
-	   crc_data <= wen ? i_crc_data : crc_data;
-    end
+	if(~i_rst_n) begin
+		cur_st <= POWER_DOWN_ST;
+	end
+	else begin
+		cur_st <= nxt_st;
+	end
 end
-	
-    
-assign o_rdata = ren ? o_reg_data : {DW{1'b0}};
-assign o_rcrc  = ren ? crc_data   : {CRC_W{1'b0}};
 
+	
 // synopsys translate_off    
 //==================================
 //assertion
 //==================================
 `ifdef ASSERT_ON
-singal_wr_rd_chk : assert property(wr_rd_chk);
-property wr_rd_chk;
-	@(posedge i_clk) (wen ^ ren);
-endproperty
+
 `endif
 // synopsys translate_on    
 endmodule
