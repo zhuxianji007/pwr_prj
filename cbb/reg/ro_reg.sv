ea25c2f7181e244266e34303a1a0c26f89e47ee0
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-06 16:08:11 +0800

    Update ro_reg.sv

--- cbb/reg/ro_reg.sv
+++ cbb/reg/ro_reg.sv
@@ -13,12 +13,14 @@ module ro_reg #(
     parameter AW                   = 8          ,
     parameter REG_ADDR             = {AW{1'b0}} ,
     parameter SUPPORT_TEST_MODE_RD = 1'b1       ,
-    parameter SUPPORT_CFG_MODE_RD  = 1'b1       , 
+    parameter SUPPORT_CFG_MODE_RD  = 1'b1       ,
+    parameter SUPPORT_SPI_EN_RD    = 1'b1       , 
     parameter END_OF_LIST          = 1
 )( 
     input  logic 		   i_ren                ,
-    input  logic           i_test_mode_status   ,
-    input  logic           i_cfg_mode_status    ,
+    input  logic           i_test_st_reg_en     ,
+    input  logic           i_cfg_st_reg_en      ,
+    input  logic           i_spi_ctrl_reg_en    ,
     input  logic [AW-1: 0] i_addr               ,
     input  logic [DW-1: 0] i_ff_data            ,//from inner logic flip_flop
     output logic [DW-1: 0] o_rdata              ,
@@ -38,7 +40,7 @@ logic hit;
 //main code
 //==================================
 assign hit = (i_addr==REG_ADDR);
-assign ren = i_ren & hit & ((i_test_mode_status & SUPPORT_TEST_MODE_RD) | (i_cfg_mode_status & SUPPORT_CFG_MODE_RD));
+assign ren = i_ren & hit & ((i_test_st_reg_en & SUPPORT_TEST_MODE_RD) | (i_cfg_st_reg_en & SUPPORT_CFG_MODE_RD) | (i_spi_ctrl_reg_en & SUPPORT_SPI_EN_RD));
    
 assign o_rdata = ren ? i_ff_data : {DW{1'b0}}; 
 
