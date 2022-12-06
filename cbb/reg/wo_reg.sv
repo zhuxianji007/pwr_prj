c4b750a39bcec290cc227121df50074d3a99bf89
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-06 16:09:38 +0800

    Update wo_reg.sv

--- cbb/reg/wo_reg.sv
+++ cbb/reg/wo_reg.sv
@@ -14,15 +14,16 @@ module wo_reg #(
     parameter DEFAULT_VAL          = {DW{1'b0}} ,
     parameter REG_ADDR             = {AW{1'b0}} ,
     parameter SUPPORT_TEST_MODE_WR = 1'b1       ,
-    parameter SUPPORT_TEST_MODE_RD = 1'b1       ,
     parameter SUPPORT_CFG_MODE_WR  = 1'b1       ,
-    parameter SUPPORT_CFG_MODE_RD  = 1'b1       , 
+    parameter SUPPORT_SPI_EN_WR    = 1'b1       ,
+    parameter SUPPORT_EFUSE_WR     = 1'b1       , 
     parameter END_OF_LIST          = 1
 )( 
     input  logic           i_wen                ,
-    input  logic           i_ren                ,
-    input  logic           i_test_mode_status   ,
-    input  logic           i_cfg_mode_status    ,
+    input  logic           i_test_st_reg_en     ,
+    input  logic           i_cfg_st_reg_en      ,
+    input  logic           i_spi_ctrl_reg_en    ,
+    input  logic           i_efuse_ctrl_reg_en  ,
     input  logic [AW-1: 0] i_addr               ,
     input  logic [DW-1: 0] i_wdata              ,
     output logic [DW-1: 0] o_rdata              ,
@@ -45,15 +46,14 @@ logic [DW-1: 0] reg_data;
 //main code
 //==================================
 assign hit = (i_addr==REG_ADDR);
-assign wen = i_wen & hit & ((i_test_mode_status & SUPPORT_TEST_MODE_WR) | (i_cfg_mode_status & SUPPORT_CFG_MODE_WR));
-assign ren = i_ren & hit & ((i_test_mode_status & SUPPORT_TEST_MODE_RD) | (i_cfg_mode_status & SUPPORT_CFG_MODE_RD));
+assign wen = i_wen & hit & ((i_test_st_reg_en & SUPPORT_TEST_MODE_WR) | (i_cfg_st_reg_en & SUPPORT_CFG_MODE_WR) | (i_spi_ctrl_reg_en & SUPPORT_SPI_EN_WR) | (i_efuse_ctrl_reg_en & SUPPORT_EFUSE_WR));
   
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
 	    reg_data <= DEFAULT_VAL;
 	end
   	else begin
-	    reg_data <= wen ? i_wdata : {DW{1'b0}};
+	    reg_data <= wen ? i_wdata : DEFAULT_VAL;
 	end
 end
 
