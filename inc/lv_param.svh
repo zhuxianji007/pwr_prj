2c121fe31c5f642f8a174941df1cb2ae8f2cf274
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-09 22:38:01 +0800

    Add files via upload

--- inc/lv_param.svh
+++ inc/lv_param.svh
@@ -1,4 +1,4 @@
-`include "comm_param.vh"
+`include "comm_param.svh"
 
 parameter CTRL_FSM_ST_NUM       = 9                                                         ,
 parameter CTRL_FSM_ST_W         = CTRL_FSM_ST_NUM ? 1 : $clog2(CTRL_FSM_ST_NUM)             ,
