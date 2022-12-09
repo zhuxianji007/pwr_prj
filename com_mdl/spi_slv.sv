35677234ab9977dc71ced8065067dae761bfbb9a
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-09 22:36:53 +0800

    Add files via upload

--- com_mdl/spi_slv.sv
+++ com_mdl/spi_slv.sv
@@ -9,7 +9,7 @@
 //1.0           2022/11/6     xxxx            Create
 //=============================================================
 module spi_slv #(
-    `include "com_param.vh"
+    `include "com_param.svh"
     parameter END_OF_LIST = 1
 )( 
     input  logic                    i_spi_sclk          ,
