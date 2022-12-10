051cd242e9f4cb02276bc02d60dd641f057a1380
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-10 15:02:28 +0800

    Update efuse_rw_ctrl.sv

--- com_mdl/efuse_rw_ctrl.sv
+++ com_mdl/efuse_rw_ctrl.sv
@@ -16,19 +16,20 @@ module efuse_rw_ctrl #(
 )( 
     input  logic                                        i_efuse_wmode       ,//1: io_efuse_setb ctrl efuse rw; 0: inner lgc ctrl efuse rw.
     input  logic                                        i_io_efuse_setb     ,//10us meas write, 500ns means read.
-    input  logic                                        i_efuse_wr_req      ,
-    input  logic                                        i_efuse_rd_req      ,
+    input  logic                                        i_efuse_wr_p        ,
+    input  logic                                        i_efuse_rd_p        ,
     input  logic [EFUSE_AW-1:       0]                  i_efuse_addr        ,
     input  logic [EFUSE_DATA_NUM-1: 0][EFUSE_DW-1: 0]   i_efuse_wdata       ,
-    output logic                                        o_efuse_done        ,
-    output logic [EFUSE_DATA_NUM-1: 0][EFUSE_DW-1: 0]   o_efuse_rdata       ,
+    output logic                                        o_efuse_op_finish   ,
+    output logic                                        o_efuse_reg_update  ,//efuse update data to reg bank.
+    output logic [EFUSE_DATA_NUM-1: 0][EFUSE_DW-1: 0]   o_efuse_reg_data    ,
 
     output logic                                        o_efuse_xxx         ,
     output logic                                        o_efuse_xxx         ,
 
     input  logic                                        i_clk               ,
     input  logic                                        i_rst_n
- );
+);
 //==================================
 //local param delcaration
 //==================================
@@ -50,4 +51,5 @@ module efuse_rw_ctrl #(
 `endif
 // synopsys translate_on    
 endmodule
+
 
