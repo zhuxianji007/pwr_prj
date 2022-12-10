3d4e277c8fd24b09d9e19a1f23b88c883ff983f0
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-10 15:00:20 +0800

    Update lv_param.svh

--- inc/lv_param.svh
+++ inc/lv_param.svh
@@ -4,3 +4,5 @@ parameter CTRL_FSM_ST_NUM       = 9                                             
 parameter CTRL_FSM_ST_W         = CTRL_FSM_ST_NUM ? 1 : $clog2(CTRL_FSM_ST_NUM)             ,
 parameter LV_SCAN_REG_NUM       = 8                                                         ,
 
+parameter EFUSE_DATA_NUM        = 8                                                         ,
+parameter EFUSE_DW              = REG_DW                                                    ,
