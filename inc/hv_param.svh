4900f01df8e169518d71ca471ed9b00ad6868df4
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-11 21:29:27 +0800

    Add files via upload

--- inc/hv_param.svh
+++ inc/hv_param.svh
@@ -1,1 +1,4 @@
 `include "comm_param.svh"
+
+parameter EFUSE_DATA_NUM        = 16                                                        ,
+parameter EFUSE_DW              = REG_DW                                                    ,
--- inc/lv_param.svh
+++ inc/lv_param.svh
@@ -2,7 +2,7 @@
 
 parameter CTRL_FSM_ST_NUM       = 9                                                         ,
 parameter CTRL_FSM_ST_W         = CTRL_FSM_ST_NUM ? 1 : $clog2(CTRL_FSM_ST_NUM)             ,
-parameter LV_SCAN_REG_NUM       = 8                                                         ,
+parameter LV_SCAN_REG_NUM       = 6                                                         ,
 
 parameter EFUSE_DATA_NUM        = 8                                                         ,
 parameter EFUSE_DW              = REG_DW                                                    ,
