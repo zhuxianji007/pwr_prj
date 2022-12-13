5dc12f4b74d3d2e3afdcab2643f00120c7a1f757
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-13 09:03:46 +0800

    Update lv_top.f

--- lv_top/lv_top.f
+++ lv_top/lv_top.f
@@ -1,4 +1,4 @@
-+incdir+$PRJ_ROOT/rtl/inc/
++incdir+$PRJ_ROOT/inc/
 
 //common
 -f $PRJ_ROOT/cbb/cbb.f
@@ -6,5 +6,16 @@
 
 //lv
 $PRJ_ROOT/lv_top/lv_pkg.sv
+$PRJ_ROOT/lv_top/lv_abist.sv
+$PRJ_ROOT/lv_top/lv_ctrl_unit.sv
+$PRJ_ROOT/lv_top/lv_dgt_pwm_ctrl.sv
+$PRJ_ROOT/lv_top/lv_hv_shadow_reg.sv
+$PRJ_ROOT/lv_top/lv_lbist.sv
+$PRJ_ROOT/lv_top/lv_owt_tx_ctrl.sv
+$PRJ_ROOT/lv_top/lv_owt_rx_ctrl.sv
+$PRJ_ROOT/lv_top/lv_pwm_int_proc.sv
+$PRJ_ROOT/lv_top/lv_pwm_intb_decode.sv
+$PRJ_ROOT/lv_top/lv_reg_access_ctrl.sv
+$PRJ_ROOT/lv_top/lv_reg_slv.sv
+$PRJ_ROOT/lv_top/lv_wdg_ctrl.sv
 $PRJ_ROOT/lv_top/lv_core.sv
-$PRJ_ROOT/lv_top/lv_fsm_ctrl.sv
