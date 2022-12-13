ac89cb9bfe44f0c7e8a9255637f774d9b4fb3e8c
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-13 09:09:29 +0800

    Update cbb.f

--- cbb/cbb.f
+++ cbb/cbb.f
@@ -1,3 +1,16 @@
--v $PRJ_ROOT/cbb/crc/xx.sv
--v $PRJ_ROOT/cbb/reg/xx.sv
--v $PRJ_ROOT/cbb/misc/xx.sv
+-v $PRJ_ROOT/cbb/crc/crc8_serial.sv
+-v $PRJ_ROOT/cbb/crc/crc8to8_parallel.sv
+-v $PRJ_ROOT/cbb/crc/crc16to8_parallel.sv
+
+-v $PRJ_ROOT/cbb/misc/gnrl_sync.sv
+-v $PRJ_ROOT/cbb/misc/rstn_merge.sv
+-v $PRJ_ROOT/cbb/misc/rstn_sync.sv
+-v $PRJ_ROOT/cbb/misc/signal_detect.sv
+-v $PRJ_ROOT/cbb/misc/signal_extend.sv
+-v $PRJ_ROOT/cbb/misc/tree_adder.sv
+
+-v $PRJ_ROOT/cbb/reg/ro_reg.sv
+-v $PRJ_ROOT/cbb/reg/rw_reg.sv
+-v $PRJ_ROOT/cbb/reg/rwc_reg.sv
+-v $PRJ_ROOT/cbb/reg/rww_reg.sv
+-v $PRJ_ROOT/cbb/reg/wo_reg.sv
