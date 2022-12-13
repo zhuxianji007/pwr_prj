a2151975252da81aa8e1211afac396aa0dad1854
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-13 20:28:31 +0800

    Create hv_top.f

---
+++ hv_top/hv_top.f
@@ -0,0 +1,21 @@
++incdir+$PRJ_ROOT/inc/
+
+//common
+-f $PRJ_ROOT/cbb/cbb.f
+-f $PRJ_ROOT/com_mdl/com_mdl.f
+
+//lv
+$PRJ_ROOT/hv_top/hv_pkg.sv
+$PRJ_ROOT/hv_top/hv_abist.sv
+$PRJ_ROOT/hv_top/hv_ctrl_unit.sv
+$PRJ_ROOT/hv_top/hv_dgt_pwm_ctrl.sv
+$PRJ_ROOT/hv_top/hv_hv_shadow_reg.sv
+$PRJ_ROOT/hv_top/hv_lbist.sv
+$PRJ_ROOT/hv_top/hv_owt_tx_ctrl.sv
+$PRJ_ROOT/hv_top/hv_owt_rx_ctrl.sv
+$PRJ_ROOT/hv_top/hv_pwm_int_proc.sv
+$PRJ_ROOT/hv_top/hv_pwm_intb_decode.sv
+$PRJ_ROOT/hv_top/hv_reg_access_ctrl.sv
+$PRJ_ROOT/hv_top/hv_reg_slv.sv
+$PRJ_ROOT/hv_top/hv_wdg_ctrl.sv
+$PRJ_ROOT/hv_top/hv_core.sv
