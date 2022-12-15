9b1d5b9e3644d4f95006bd10ebb34c07f2c89151
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-15 20:57:41 +0800

    Update lv_reg_access_ctrl.sv

--- lv_top/lv_reg_access_ctrl.sv
+++ lv_top/lv_reg_access_ctrl.sv
@@ -34,8 +34,9 @@ module lv_reg_access_ctrl #(
     output logic [REG_AW-1:     0]  o_spi_owt_addr          ,
     output logic [REG_DW-1:     0]  o_spi_owt_data          ,
     input  logic                    i_owt_tx_spi_ack        ,
-    input  logic                    i_owt_rx_spi_rsp        ,
     output logic                    o_spi_rst_wdg           ,
+
+    input  logic                    i_owt_rx_spi_rsp        ,
 
     output logic                    o_rac_reg_ren           ,
     output logic                    o_rac_reg_wen           ,
@@ -48,51 +49,51 @@ module lv_reg_access_ctrl #(
     input  logic [REG_DW-1:     0]  i_reg_rac_rdata         ,
     input  logic [REG_CRC_W-1:  0]  i_reg_rac_rcrc          ,
 
+    input  logic                    i_hv_reg_vld            ,
+    input  logic [REG_DW-1:     0]  i_hv_ang_reg_data       ,
+
     input  logic                    i_clk                   ,
     input  logic                    i_rst_n
  );
 //==================================
 //local param delcaration
 //==================================
-localparam HV_ANALOG_REG_START_ADDR              = 7'h40                                                    ;
-localparam HV_ANALOG_REG_END_ADDR                = 7'h6E                                                    ;
-localparam COM_WR_REG_NUM                        = 9                                                        ;
-localparam COM_RD_REG_NUM                        = 9                                                        ;
-localparam [COM_WR_REG_NUM-1: 0] COM_WR_REG_ADDR = {7'h0B,7'h0A,7'h09,7'h08,7'h07,7'h06,7'h03,7'h02,7'h01}  ;
-localparam [COM_RD_REG_NUM-1: 0] COM_RD_REG_ADDR = {7'h1F,7'h15,7'h14,7'h0D,7'h0C,7'h0A,7'h08,7'h07,7'h06}  ;
+localparam COM_WR_REG_NUM                                = 9                                                        ;
+localparam COM_RD_REG_NUM                                = 9                                                        ;
+localparam integer COM_WR_REG_ADDR[COM_WR_REG_NUM-1: 0]  = {7'h0B,7'h0A,7'h09,7'h08,7'h07,7'h06,7'h03,7'h02,7'h01}  ;
+localparam integer COM_RD_REG_ADDR[COM_RD_REG_NUM-1: 0]  = {7'h1F,7'h15,7'h14,7'h0D,7'h0C,7'h0A,7'h08,7'h07,7'h06}  ;
 //==================================
 //var delcaration
 //==================================
 logic                                       wdg_scan_grant          ;
-logic [2:                   0]              wdg_scan_grant_ff       ;
+logic [1:                   0]              wdg_scan_grant_ff       ;
 logic                                       wdg_scan_grant_mask     ;
 logic                                       trig_owt_wr_dgt_reg     ;
 logic                                       trig_owt_rd_dgt_reg     ;//digital
 logic                                       trig_owt_acc_ang_reg    ;//analog
 logic                                       owt_wr_ack              ;
 logic                                       owt_rd_ack              ;
-logic                                       spi_rac_wr_req_ff       ;
-logic                                       spi_rac_rd_req_ff       ;
 logic [REG_DW-1:            0]              rac_spi_data            ;
-logic                                       owt_rx_spi_rsp_ff       ;                 
+logic                                       spi_owt_wr_req_ff       ;
+logic                                       spi_owt_rd_req_ff       ;                 
 //==================================
 //main code
 //==================================
-assign wdg_scan_grant_mask  = ~(|wdg_scan_grant_ff[2: 1])                                                          ;
+assign wdg_scan_grant_mask  = ~(|wdg_scan_grant_ff[1: 1])                                                          ;
 assign wdg_scan_grant       = i_wdg_scan_rac_rd_req & ~(i_spi_rac_wr_req | i_spi_rac_rd_req) & wdg_scan_grant_mask ;
 
 assign wdg_scan_grant_ff[0] = wdg_scan_grant;
 
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-        wdg_scan_grant_ff[2: 1] <= 2'b0;
+        wdg_scan_grant_ff[1: 1] <= 1'b0;
     end
     else begin
-        wdg_scan_grant_ff[2: 1] <= wdg_scan_grant_ff[1: 0];
+        wdg_scan_grant_ff[1: 1] <= wdg_scan_grant_ff[0: 0];
     end
 end
 
-assign o_rac_wdg_scan_ack  = i_reg_rac_rack & wdg_scan_grant_ff[2]  ;
+assign o_rac_wdg_scan_ack  = i_reg_rac_rack & wdg_scan_grant_ff[1]  ;
 assign o_rac_wdg_scan_data = i_reg_rac_rdata                        ;
 assign o_rac_wdg_scan_crc  = i_reg_rac_rcrc                         ;
 
@@ -112,55 +113,25 @@ always_comb begin: TRIG_OWT_RD_DGT_REG_BLK
     end
 end
 
+assign o_spi_owt_wr_req = (trig_owt_wr_dgt_reg | trig_owt_acc_ang_reg) & i_spi_rac_wr_req;
+
+assign o_spi_owt_rd_req = (trig_owt_rd_dgt_reg | trig_owt_acc_ang_reg) & i_spi_rac_rd_req;
+
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-        spi_rac_wr_req_ff <= 1'b0;
+        spi_owt_wr_req_ff <= 1'b0;
     end
     else begin
-        spi_rac_wr_req_ff <= i_spi_rac_wr_req;
+        spi_owt_wr_req_ff <= o_spi_owt_wr_req;
     end
 end
 
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-        spi_rac_rd_req_ff <= 1'b0;
+        spi_owt_rd_req_ff <= 1'b0;
     end
     else begin
-        spi_rac_rd_req_ff <= i_spi_rac_rd_req;
-    end
-end
-
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_spi_owt_wr_req <= 1'b0;
-    end
-    else if(i_owt_tx_spi_ack) begin
-        o_spi_owt_wr_req <= 1'b0;
-    end
-    else begin
-        o_spi_owt_wr_req <= (trig_owt_wr_dgt_reg | trig_owt_acc_ang_reg) & i_spi_rac_wr_req;
-    end
-end
-
-
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        owt_rx_spi_rsp_ff <= 1'b0;
-    end
-    else begin
-        owt_rx_spi_rsp_ff <= i_owt_rx_spi_rsp;
-    end
-end
-
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_spi_owt_rd_req <= 1'b0;
-    end
-    else if(i_owt_tx_spi_ack) begin
-        o_spi_owt_rd_req <= 1'b0;
-    end
-    else begin
-        o_spi_owt_rd_req <= (trig_owt_rd_dgt_reg | trig_owt_acc_ang_reg) & i_spi_rac_rd_req;
+        spi_owt_rd_req_ff <= o_spi_owt_rd_req;
     end
 end
 
@@ -168,10 +139,7 @@ always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
         o_spi_rst_wdg <= 1'b0;
     end
-    else if((trig_owt_rd_dgt_reg | trig_owt_acc_ang_reg) & (i_spi_rac_rd_req & ~spi_rac_rd_req_ff)) begin
-        o_spi_rst_wdg <= 1'b1;
-    end
-    else if((trig_owt_wr_dgt_reg | trig_owt_acc_ang_reg) & (i_spi_rac_wr_req & ~spi_rac_wr_req_ff)) begin
+    else if((o_spi_owt_wr_req & ~spi_owt_wr_req_ff) || (o_spi_owt_rd_req & ~spi_owt_rd_req_ff)) begin
         o_spi_rst_wdg <= 1'b1;
     end
     else begin
@@ -179,81 +147,27 @@ always_ff@(posedge i_clk or negedge i_rst_n) begin
     end
 end
 
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_spi_owt_addr <= REG_AW'(0);
-    end
-    else if((i_spi_rac_rd_req|i_spi_rac_wr_req) & (trig_owt_wr_dgt_reg|trig_owt_rd_dgt_reg|trig_owt_acc_ang_reg)) begin
-        o_spi_owt_addr <= i_spi_rac_addr;
-    end
-end
+assign o_spi_owt_addr = i_spi_rac_addr;
 
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_spi_owt_data <= REG_DW'(0);
-    end
-    else if(i_spi_rac_wr_req & (trig_owt_wr_dgt_reg|trig_owt_acc_ang_reg)) begin
-        o_spi_owt_data <= i_spi_rac_wdata;
-    end
-end
+assign o_spi_owt_data = i_spi_rac_wdata;
 
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_rac_reg_ren <= 1'b0;
-    end
-    else begin
-        o_rac_reg_ren <= i_spi_rac_rd_req | wdg_scan_grant;
-    end
-end
+assign o_rac_reg_ren  = i_spi_rac_rd_req | wdg_scan_grant;
 
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_rac_reg_wen <= 1'b0;
-    end
-    else begin
-        o_rac_reg_wen <= i_spi_rac_wr_req;
-    end
-end
+assign o_rac_reg_wen  = i_spi_rac_wr_req;
 
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_rac_reg_addr <= REG_AW'(0);
-    end
-    else if(i_spi_rac_wr_req|i_spi_rac_rd_req) begin
-        o_rac_reg_addr <= i_spi_rac_addr;
-    end
-    else if(wdg_scan_grant) begin
-        o_rac_reg_addr <= i_wdg_scan_rac_addr;
-    end
-    else;
-end
+assign o_rac_reg_addr = (i_spi_rac_wr_req | i_spi_rac_rd_req) ? i_spi_rac_addr : i_wdg_scan_rac_addr;
 
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_rac_reg_wdata <= REG_DW'(0);
-    end
-    else if(i_spi_rac_wr_req) begin
-        o_rac_reg_wdata <= i_spi_rac_wdata;
-    end
-    else;
-end
+assign o_rac_reg_wdata= i_spi_rac_wdata; 
 
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-        o_rac_reg_wcrc <= REG_CRC_W'(0);
-    end
-    else if(i_spi_rac_wr_req) begin
-        o_rac_reg_wcrc <= i_spi_rac_wcrc;
-    end
-    else;
-end
+assign o_rac_reg_wcrc = i_spi_rac_wcrc;
                        
-assign rac_spi_data    = i_spi_rac_wr_req ? o_rac_reg_wdata : i_reg_rac_rdata;
+assign rac_spi_data    = i_spi_rac_wr_req ? o_rac_reg_wdata : 
+                         (trig_owt_acc_ang_reg ? i_hv_ang_reg_data : i_reg_rac_rdata);
 
-assign o_rac_spi_wack = i_owt_tx_spi_ack  | (~trig_owt_wr_dgt_reg & ~trig_owt_acc_ang_reg & i_reg_rac_wack)                        ; 
-assign o_rac_spi_rack = owt_rx_spi_rsp_ff | (~trig_owt_rd_dgt_reg & ~trig_owt_acc_ang_reg & i_reg_rac_rack & ~wdg_scan_grant_ff[2]);
-assign o_rac_spi_data = rac_spi_data                                                                                             ;
-assign o_rac_spi_addr = o_rac_reg_addr                                                                                           ;
+assign o_rac_spi_wack = i_owt_tx_spi_ack  | (~trig_owt_wr_dgt_reg & ~trig_owt_acc_ang_reg & i_reg_rac_wack)                         ; 
+assign o_rac_spi_rack = i_hv_reg_vld      | (~trig_owt_rd_dgt_reg & ~trig_owt_acc_ang_reg & i_reg_rac_rack & ~wdg_scan_grant_ff[1]) ;
+assign o_rac_spi_data = rac_spi_data                                                                                                ;
+assign o_rac_spi_addr = o_rac_reg_addr                                                                                              ;
 // synopsys translate_off    
 //==================================
 //assertion
@@ -261,5 +175,8 @@ assign o_rac_spi_addr = o_rac_reg_addr                                          
 //    
 // synopsys translate_on    
 endmodule
+
+
+
 
 
