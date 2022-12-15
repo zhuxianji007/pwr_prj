187d8403e9124fdfb1a394df2c6ba37bff8374b8
zhuxianji007 <117557679+zhuxianji007@users.noreply.github.com>
2022-12-15 21:07:29 +0800

    Update lv_top.f

--- lv_top/lv_top.f
+++ lv_top/lv_top.f
@@ -13,7 +13,7 @@ $PRJ_ROOT/lv_top/lv_hv_shadow_reg.sv
 $PRJ_ROOT/lv_top/lv_lbist.sv
 $PRJ_ROOT/lv_top/lv_owt_tx_ctrl.sv
 $PRJ_ROOT/lv_top/lv_owt_rx_ctrl.sv
-$PRJ_ROOT/lv_top/lv_pwm_int_proc.sv
+$PRJ_ROOT/lv_top/lv_analog_int_proc.sv
 $PRJ_ROOT/lv_top/lv_pwm_intb_decode.sv
 $PRJ_ROOT/lv_top/lv_reg_access_ctrl.sv
 $PRJ_ROOT/lv_top/lv_reg_slv.sv
